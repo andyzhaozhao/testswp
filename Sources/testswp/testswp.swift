1cde7666d203f87f6f9008d0c288d8b5f8d855d0
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2020-11-04 20:59:08 +0800

    init

--- Sources/testswp/testswp.swift
+++ Sources/testswp/testswp.swift
@@ -1,3 +1,7 @@
-struct testswp {
-    var text = "Hello, World!"
+public struct testswp {
+    public var text = "Hello, World!"
+    
+    public func testt(a:String) -> String {
+        return "fanhui: \(a)"
+    }
 }
